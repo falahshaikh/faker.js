56ab2d5679cc6880bc9aa41fc8c4716372f6f53e
fala <shaikh_falah@hotmail.com>
2019-07-09 16:42:55 +0530

    Create app.js
    app.js to consume faker.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var faker = require('./faker.js');
+var Star = faker.celestial.stars();
+console.log(Star);
+var Galaxy = faker.celestial.galaxies();
+console.log(Galaxy);
+var Planets = faker.celestial.planets();
+console.log(Planets);
+var Comets = faker.celestial.comets();
+console.log(Comets);
